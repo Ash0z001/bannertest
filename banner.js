e29a209c12b61b84da4bf0cb20221e09fe3c29ad
Ash0z001 <ashdevelop01@gmail.com>
2021-07-20 14:00:01 +0200

    Pulled content box higher

--- banner.js
+++ banner.js
@@ -48,7 +48,7 @@ function initHandler() { //Subsequent loading...
         attr: { id:"requestARep", src:"request-a-rep.png"} });
 
     m.create("div", {
-        css: { x:0, y:_height, width:299, height:201, autoAlpha:0},
+        css: { x:0, y:_height, width:299, height:50, autoAlpha:0},
         attr: { id:"isi-col", class:"isi-content"},
         text: mkr.query("#isi-content").innerHTML });
 
